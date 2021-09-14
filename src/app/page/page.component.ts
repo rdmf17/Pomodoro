07804cac2239a3a46bd6075668b5a6149ded3c70
rdmf17 <rdmf17rdmf@gmail.com>
2021-09-14 21:27:53 +0100

    EarlyChanges

---
+++ src/app/page/page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.scss']
+})
+export class PageComponent implements OnInit {
+
+  videoSrc= '../../assets/videos/video.mp4'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
